aa2b3e1a8c2e893a0dd0a92dc17d7f6a3647bcd4
damnbruuh <61203332+damnbruuh@users.noreply.github.com>
2020-02-28 23:56:03 -0500

    Update tcp_client.py

--- tcp_client.py
+++ tcp_client.py
@@ -1,5 +1,7 @@
 # when testing, ensure you run server and client in separate interpreters
 # also ensure server runs before client
+#to do:
+#decode server packet from utf-8 to ascii
 
 import socket
 import sys
